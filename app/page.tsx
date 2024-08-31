a4d8886089adcf386224718178e19133694886ac
sarthak0714 <sarthak.t1403@gmail.com>
2024-09-01 02:25:34 +0530

    added font and placeholder text

--- app/page.tsx
+++ app/page.tsx
@@ -3,44 +3,79 @@ export default function Home() {
     <main className="border-2 border-white m-2">
       <div className="grid grid-rows-2 md:grid-cols-3 md:grid-rows-1">
         <div className="border-b-2 border-white h-14 grid grid-cols-12 gap-0 md:col-span-2">
-          <div className=" border-r-2 border-white h-full col-span-2 md:col-span-1"></div>
-          <div className="h-full col-span-10"></div>
+          <div className=" border-r-2 border-white h-full col-span-2 md:col-span-1">
+            <p className="text-white text-center mt-3">0714</p>
+          </div>
+          <div className="h-full col-span-10">
+            <p className="text-white text-3xl pl-10 pt-2 ">Sarthak Tanpure.</p>
+          </div>
         </div>
         <div className="h-10 border-b-2 border-white grid grid-cols-4 md:h-14">
           <div className="border-r-2 border-white"></div>
-          <div className="border-r-2 border-white"></div>
-          <div className="border-r-2 border-white"></div>
-          <div className=""></div>
-        </div>
-      </div>
-      <div className="border-b-2 border-white grid grid-rows-2 h-96 md:grid-cols-2 md:grid-rows-1 md:h-56 ">
-        <div className="border-b-2 md:border-b-0 border-white md:border-r-2"></div>
-        <div className=" grid grid-cols-2  md:border-white">
-          <div className="border-r-2 border-white "></div>
-          <div className="grid grid-rows-4 md:grid-rows-3">
-            <div className="border-b-2 border-white"></div>
-            <div className="border-b-2 row-span-2 border-white md:row-span-1"></div>
-            <div className=""></div>
+          <div className="border-r-2 border-white">
+            {" "}
+            <p className="text-white">Exp.</p>
+          </div>
+          <div className="border-r-2 border-white">
+            <p className="text-white">Wins.</p>
+          </div>
+          <div className="">
+            <p className="text-white">Projects</p>
           </div>
         </div>
       </div>
-      <div className="border-b-2 border-white h-8"></div>
+      <div className="border-b-2 border-white grid grid-rows-2 h-96 md:grid-cols-2 md:grid-rows-1 md:h-56 ">
+        <div className="border-b-2 md:border-b-0 border-white md:border-r-2">
+          <p className="text-white">Hi i am sarthak. Cyka Blyat</p>
+        </div>
+        <div className=" grid grid-cols-2  md:border-white">
+          <div className="border-r-2 border-white ">
+            <p className="text-white">Map here</p>
+          </div>
+          <div className="grid grid-rows-4 md:grid-rows-3">
+            <div className="border-b-2 border-white">
+              <p className="text-white">Rolling tech stack</p>
+            </div>
+            <div className="border-b-2 row-span-2 border-white md:row-span-1">
+              <p className="text-white">Socials</p>
+            </div>
+            <div className="">
+              <p className="text-white">Resume link</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-b-2 border-white h-6 diagonal-stripes "></div>
       <div className="grid grid-rows-2 border-b-2 border-white h-96 md:h-56 md:grid-rows-1 md:grid-cols-2">
-        <div className="border-b-2 border-white md:border-b-0 md:border-r-2"></div>
-        <div className=""></div>
+        <div className="border-b-2 border-white md:border-b-0 md:border-r-2">
+          <p className="text-white">Jobs</p>
+        </div>
+        <div className="">
+          <p className="text-white">Winners</p>
+        </div>
       </div>
-      <div className="border-b-2 border-white h-8"></div>
+      <div className="border-b-2 border-white h-6 diagonal-stripes"></div>
       <div className="border-b-2 border-white h-[42rem] md:h-56 grid grid-rows-3 md:grid-rows-1 md:grid-cols-3">
-        <div className="border-b-2 border-white h-56 md:border-b-0 md:border-r-2"></div>
-        <div className="border-b-2 border-white h-56 md:border-b-0 md:border-r-2"></div>
-        <div className="h-56"></div>
+        <div className="border-b-2 border-white h-56 md:border-b-0 md:border-r-2">
+          <p className="text-white">proj</p>
+        </div>
+        <div className="border-b-2 border-white h-56 md:border-b-0 md:border-r-2">
+          <p className="text-white">proj</p>
+        </div>
+        <div className="h-56">
+          <p className="text-white">proj</p>
+        </div>
       </div>
-      <div className="border-b-2 border-white h-10 grid grid-cols-10 ">
-        <div className="col-span-3 border-r-2 border-white md:col-span-4"></div>
-        <div className="col-span-4 border-r-2 border-white md:col-span-2"></div>
-        <div className="col-span-3 md:col-span-4"></div>
+      <div className="border-b-2 border-white h-6 grid grid-cols-10 ">
+        <div className="col-span-3 border-r-2 border-white md:col-span-4 diagonal-stripes"></div>
+        <div className="col-span-4 border-r-2 border-white md:col-span-2 ">
+          <p className="text-white">More</p>
+        </div>
+        <div className="col-span-3 md:col-span-4 diagonal-stripes"></div>
       </div>
-      <div className="h-8"></div>
+      <div className="h-8 p-1">
+        <p className="text-white text-center">Made with 💗 by Sarthak</p>
+      </div>
     </main>
   );
 }
